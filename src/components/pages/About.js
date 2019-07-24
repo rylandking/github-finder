cfa7c10d284b1db05c36823750b054b4877508b2
rylandking <ryland@users.noreply.github.com>
2019-07-24 07:42:05 -0600

    Complete project before refactor with hooks and context

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>This app searches Github users.</p>
+      <p>v1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
